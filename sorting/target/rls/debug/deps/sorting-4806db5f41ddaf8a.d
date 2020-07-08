d15677357fee6cbf686ad3c96711ee3c40fce732
Dan O'Connor <oconnor.dmoc@gmail.com>
2020-07-08 14:14:34 +0100

    final attempt at removing target dir

--- sorting/target/rls/debug/deps/sorting-4806db5f41ddaf8a.d
+++
@@ -1,7 +0,0 @@
-/home/dmoc/code/repos/algorithms/sorting/target/rls/debug/deps/sorting-4806db5f41ddaf8a.rmeta: src/main.rs src/quick_sort_int_array.rs src/heap_sort.rs
-
-/home/dmoc/code/repos/algorithms/sorting/target/rls/debug/deps/sorting-4806db5f41ddaf8a.d: src/main.rs src/quick_sort_int_array.rs src/heap_sort.rs
-
-src/main.rs:
-src/quick_sort_int_array.rs:
-src/heap_sort.rs:
--- sorting/target/rls/debug/deps/sorting-df0e6c9626901f77.d
+++
@@ -1,7 +0,0 @@
-/home/dmoc/code/repos/algorithms/sorting/target/rls/debug/deps/sorting-df0e6c9626901f77.rmeta: src/main.rs src/quick_sort_int_array.rs src/heap_sort.rs
-
-/home/dmoc/code/repos/algorithms/sorting/target/rls/debug/deps/sorting-df0e6c9626901f77.d: src/main.rs src/quick_sort_int_array.rs src/heap_sort.rs
-
-src/main.rs:
-src/quick_sort_int_array.rs:
-src/heap_sort.rs:

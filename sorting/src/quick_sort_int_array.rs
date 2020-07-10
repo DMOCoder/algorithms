749781367cd26c49f330cbd2495be8fc0393458c
Dan O'Connor <oconnor.dmoc@gmail.com>
2020-07-10 21:13:23 +0100

    add tests for quick sort int

--- sorting/src/quick_sort_int_array.rs
+++ sorting/src/quick_sort_int_array.rs
@@ -35,7 +35,15 @@ fn partition(arr: &mut [i32], low: usize, high: usize) -> usize {
         if left <= right {
             swap_index(arr, left, right);
             left += 1;
-            right -= 1;
+            
+            //for certain arrays, this is called when right is zero and
+            //the application panics because usize cannot be negative
+            //this wouldn't be a problem for a c style app using an int 
+            //for range but given rust forces us to use usize we encounter
+            //this problem
+            if 0 < right {
+                right -= 1;
+            }
         }
     }
 
@@ -46,4 +54,51 @@ fn swap_index(arr: &mut [i32], first: usize, second: usize) {
     let swap = arr[first];
     arr[first] = arr[second];
     arr[second] = swap;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::Rng;
+
+    fn validate_array_is_sorted(arr: &mut [i32]) {
+        let mut prev_val = 0;
+
+        for i in 0..arr.len() {
+            if 0 < i {
+                if arr[i] < prev_val {
+                    assert!(false, "array is not sorted!");
+                    break;
+                }
+            }
+            
+            prev_val = arr[i];
+        }
+    }
+
+    #[test]
+    fn array_is_sorted() {
+        let mut arr = [2, 6, 6, 5, 3, 7, 9, 1, 4, 9, 5];
+        sort_int_array(&mut arr);
+
+        validate_array_is_sorted(&mut arr);        
+    }
+
+    #[test]
+    fn random_array_is_sorted() {
+        let mut rng = rand::thread_rng();
+
+        //let len: usize = rng.gen();
+        //we cant dynamically set an array's length
+        //arrays must have a defined length at compile time :()
+        let mut arr = [0; 100];
+
+        for i in 0..arr.len() {
+            let val: i32 = rng.gen();
+            arr[i] = val;
+        }
+        sort_int_array(&mut arr);
+
+        validate_array_is_sorted(&mut arr);        
+    }
 }
